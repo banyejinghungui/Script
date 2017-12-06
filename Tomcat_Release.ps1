3c32dc2590ba32d4fb6ef23eba2ba022c48e4103
banyejinghungui <346535763@qq.com>
2017-12-06 15:09:09 -0500

    v1.1
    Add more description

--- Tomcat_Release.ps1
+++ Tomcat_Release.ps1
@@ -1,5 +1,28 @@
+#++++++++++++++++++++++ Script Start ++++++++++++++++++++++++
+# ======================= Instruction ================================
+#            FILE:                      Tomcat_Release.ps1
+#            USAGE:
+
+#            DESCRIPTION:             Releasing Tomcat
+#            OPTIONS:--
+#            REQUIREMENTS:
+#            BUGS:
+#            NOTES:
+#            AUTHOR:                  Chen
+#            CREATE:                  2017.12.06
+#            REVISION:                V1.0
+#            HISTORY：                
+#
+#
+#            Test:
+#            PROCESURE：
+# ========================================================
+
+cd C:\tmp
 git clone https://github.com/banyejinghungui/ROOT.git
 & C:\Tomcat8\bin\shutdown.bat
 Remove-Item C:\Tomcat8\webapps\ROOT -Force -Recurse
 Move-Item C:\tmp\ROOT C:\Tomcat8\webapps\
 & C:\Tomcat8\bin\startup.bat
+
+#++++++++++++++++++++++ Script End ++++++++++++++++++++++++
